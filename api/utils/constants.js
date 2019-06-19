98462ec54cd5ac2d618209373bcef751d4ddd3fc
Mark Goldberg <mark@hackeryou.com>
2019-06-19 13:03:49 -0400

    Fixing inconsistencies with naming

--- api/utils/constants.js
+++ api/utils/constants.js
@@ -1,5 +1,5 @@
 'use strict';
 
-exports.URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/jobbie'
+exports.URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/jawber'
 exports.PORT = process.env.PORT || 4000;
 exports.SECRET = process.env.SECRET || 'super-secret-passphrase';
